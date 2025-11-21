d244215e71267278ac5bdc5c89bad7b40347bde9
Leandro Barbosa <leobarbosacontact@gmail.com>
2025-11-21 15:05:53 +0100

    force ravalidity

--- nextjs-boy-art-cad/src/app/cassettes/page.tsx
+++ nextjs-boy-art-cad/src/app/cassettes/page.tsx
@@ -1,8 +1,9 @@
-import {notFound} from 'next/navigation'
-import {getProductPageByType} from '../../sanity/client'
+import { notFound } from 'next/navigation'
+import type { ProductPageData } from '../../lib/types/sanity'
+import { getProductPageByType } from '../../sanity/client'
 import ProductPage from '../components/ProductPage'
-import type {ProductPageData} from '../../lib/types/sanity'
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default async function CassetePage() {
   const data: ProductPageData = await getProductPageByType('cassettes')
